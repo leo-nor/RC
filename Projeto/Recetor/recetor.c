50179f439f317e3a5cebe3dfaacdf0702a7b9bcf
José Rodrigues <nepsname@gmail.com>
2021-11-10 12:29:54 +0000

    Add files via upload

--- Projeto/Recetor/recetor.c
+++ Projeto/Recetor/recetor.c
@@ -9,129 +9,100 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
+#include "../utils.c"
 
-#define FLAG 0x7E
 #define CMD_SEND 0x01
 #define RES_SEND 0x03
 #define CMD_REC 0x03
 #define RES_REC 0x01
-#define SET 0x03
-#define UA 0x07
 
 volatile int STOP = FALSE, ERROR = FALSE;
 
-void send_UA(int fd) {
-  char buf[255];
+int trama_num = 1;
 
-  buf[0] = FLAG;
-  buf[1] = RES_SEND;
-  buf[2] = UA;
-  buf[3] = RES_SEND ^ UA;
-  buf[4] = FLAG;
+int main(int argc, char** argv) {
+  int fd;
+  struct termios oldtio,newtio;
 
-  write(fd, buf, 5);
-  printf("UA command sent\n");
-  printf("%d bytes written\n", 5);
-}
-
-int main(int argc, char** argv)
-{
-    int fd;
-    struct termios oldtio,newtio;
-
-    if ( (argc < 2) ||
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
-      exit(1);
-    }
-
+  if ( (argc < 2) ||
+        ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+        (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
+    exit(1);
+  }
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 5;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-
-
+  newtio.c_cc[VTIME]    = 5;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
   /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
 
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    tcflush(fd, TCIOFLUSH);
+  printf("New termios structure set\n");
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
+  char buf[255];
+  int counter = 0;
+
+  while (STOP==FALSE && ERROR==FALSE) {
+    read(fd, buf + counter, 1);   /* returns after 5 chars have been input */
+    switch (counter) {
+      case 0:
+        if(buf[0] != FLAG) ERROR = TRUE;
+        break;
+      case 1:
+        if(buf[1] != CMD_REC) ERROR = TRUE;
+        break;
+      case 2:
+        if(buf[2] != SET) ERROR = TRUE;
+        break;
+      case 3:
+        if(buf[3] != (CMD_REC^SET)) ERROR = TRUE;
+        break;
+      case 4:
+        if(buf[4] != FLAG) ERROR = TRUE;
+        break;
     }
+    if(counter == 4) STOP = TRUE;
+    else counter++;
+  }
 
-    printf("New termios structure set\n");
-
-    char buf[255];
-    int counter = 0;
-
-    while (STOP==FALSE && ERROR==FALSE) {
-      read(fd, buf + counter, 1);   /* returns after 5 chars have been input */
-      switch (counter) {
-        case 0:
-          if(buf[0] != FLAG) ERROR = TRUE;
-          break;
-        case 1:
-          if(buf[1] != CMD_REC) ERROR = TRUE;
-          break;
-        case 2:
-          if(buf[2] != SET) ERROR = TRUE;
-          break;
-        case 3:
-          if(buf[3] != (CMD_REC^SET)) ERROR = TRUE;
-          break;
-        case 4:
-          if(buf[4] != FLAG) ERROR = TRUE;
-          break;
-      }
-      if(counter == 4) STOP = TRUE;
-      else counter++;
-    }
-
-    if(ERROR == TRUE) printf("Command received is not a SET\n");
-    else send_UA(fd);
-
-
+  if(ERROR == TRUE) printf("Command received is not a SET\n");
+  else send_UA(fd);
 
   /*
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o
   */
 
-
-
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
-    return 0;
+  tcsetattr(fd,TCSANOW,&oldtio);
+  close(fd);
+  return 0;
 }
---
+++ Projeto/utils.c
@@ -0,0 +1,53 @@
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define RETRY_ATTEMPTS 3
+
+#define FLAG 0x7E
+#define SET 0x03
+#define UA 0x07
+#define DISC 0x0B
+#define RR 0x05
+#define REJ 0x01
+#define ESCAPE 0x7D
+#define ESCAPED_FLAG 0x5E
+#define ESCAPED_ESCAPE 0x5D
+
+void send_trama_S(int fd, char C, char SEND, int trama_num) {
+  write(fd, create_trama_S(C, SEND, trama_num), 5);
+  switch (C) {
+  case SET:
+    printf("SET command sent\n");
+    break;
+  case UA:
+    printf("UA response sent\n");
+    break;
+  case DISC:
+    printf("DISC command sent\n");
+    break;
+  }
+}
+
+char create_trama_S(char C, char SEND, int trama_num) {
+  char buf[5];
+
+  buf[0] = FLAG;
+  buf[1] = SEND;
+  if((C == RR || C == REJ) && get_Nr(trama_num) == 1) buf[2] = C ^ 0x80;
+  else buf[2] = C;
+  buf[3] = SEND ^ C;
+  buf[4] = FLAG;
+
+  return buf;
+}
+
+int get_Ns(int t_num) {
+  if(t_num % 2 == 0) return 1;
+  else return 0;
+}
+
+int get_Nr(int t_num) {
+  if(t_num % 2 == 0) return 0;
+  else return 1;
+}
